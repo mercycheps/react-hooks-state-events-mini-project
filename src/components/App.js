26a2c44f42e32afc306fe61cc65c99f8b50da992
Mercy <maritimmercy8@gmail.com>
2025-04-17 11:46:59 +0300

    added tasklist $ div

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const handleTaskDelete =(textToRemove)=> {
+    setTasks(tasks.filter(task=>task.text !==textToRemove));
+  };
+  
+  const handleCategorySelect = (category) =>{
+    setSelectedCategory(category);
+  };
+
+  handleTaskFormSubmit = (newTask) => {
+    setTasks([...tasks.newTask]);
+  };
+  const filteredTask = selectedCategory === "ALL"
+  ?tasks : tasks.filter(task => task.category ===selectedCategory);
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
 
 function App() {
   return (
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks,onDeleteTask }) {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
+      {tasks.map((task) => (
+        <Task
+        {/* display a list of tasks using Task component */}
+          key={task.text} 
+          text={task.text}
+          category={task.category}
+          onDeleteTask={onDeleteTask}
+        />
+      ))}
+       </div>
   );
 }
 
