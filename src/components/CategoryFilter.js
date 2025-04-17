c79d0f473e246c6326abce54e9f80edc93d5a8ed
Mercy <maritimmercy8@gmail.com>
2025-04-17 12:14:38 +0300

    added category filter nand onclick n key

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter( {categories, selectedCategory, onSelectCategory }) {
   return (
     <div className="categories">
-      <h5>Category filters</h5>
+    {categories.map((category) => (
+      <button
+        key={category}
+        className={selectedCategory === category ? "selected" : ""}
+        onClick={() => onSelectCategory(category)}
+        >
+          {category}
+      </button>
+    ))}
+      {/* <h5>Category filters</h5> */}
       {/* render <button> elements for each category here */}
     </div>
   );
